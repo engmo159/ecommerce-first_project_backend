06279afa5c7a0dff148ba678e60b8242bd01c770
Mohammed <engmo159@git.com>
2024-09-09 04:44:57 +0300

    gsds

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -17,6 +17,16 @@ router.get('/', async (req, res) => {
     return res.status(500).send('something went wrong!')
   }
 })
+
+router.get('/last-product', async (req, res) => {
+  try {
+    const { data, statusCode } = await getLastProduct()
+    res.status(statusCode).send(data)
+  } catch (error) {
+    console.error('Error in /last-product route:', error)
+    res.status(500).send('Internal Server Error')
+  }
+})
 router.get('/:id', async (req, res) => {
   try {
     const products = await getProductById(req, res)
@@ -48,11 +58,6 @@ router.post('/add', async (req, res) => {
   } catch (err) {
     return res.status(500).send('something went wrong!')
   }
-})
-
-router.get('/last-product', async (req, res) => {
-  const { data, statusCode } = await getLastProduct()
-  return res.status(statusCode).send(data)
 })
 
 export default router
